39a7bce164f9338e68053de97f0802e33c3b11d3
Bojan Misic <misic.bojan@gmail.com>
2017-10-22 18:06:26 +0200

    Add missing files to Samples.Basic.XamarinForms project

---
+++ src/Samples/Basic/OpenMVVM.Samples.Basic.XamarinForms/DetailView.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace OpenMVVM.Samples.Basic.XamarinForms
+{
+    using OpenMVVM.Core;
+    using OpenMVVM.Samples.Basic.ViewModel;
+
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailView : ContentPage
+    {
+        public DetailView()
+        {
+            this.InitializeComponent();
+            this.BindingContext = ViewModelLocatorBase.InstanceFactory.GetInstance<DetailViewModel>();
+        }
+    }
+}
