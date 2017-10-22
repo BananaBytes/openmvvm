0b48968ba71e817d80c4ab4549977f25ac53871d
Bojan Misic <misic.bojan@gmail.com>
2017-10-22 18:06:26 +0200

    Add missing files to Samples.Basic.XamarinForms project

---
+++ src/Samples/Basic/OpenMVVM.Samples.Basic.XamarinForms/MainView.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace OpenMVVM.Samples.Basic.XamarinForms
+{
+    using OpenMVVM.Core;
+    using OpenMVVM.Samples.Basic.ViewModel;
+
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainView : ContentPage
+    {
+        public MainView()
+        {
+            this.InitializeComponent();
+            this.BindingContext = ViewModelLocatorBase.InstanceFactory.GetInstance<MainViewModel>();
+        }
+
+        private void ListView_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            ((MainViewModel)this.BindingContext).NavigateToItemCommand.Execute(e.SelectedItem);
+        }
+    }
+}
